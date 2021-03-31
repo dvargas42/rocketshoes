31c3da9a9d4a48e6ede8951a988435e932be6494
dvargas42 <dvargas.eng@gmail.com>
2021-03-31 14:19:12 -0300

    rendering of product array

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -44,7 +44,30 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-     
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>
+            {new Intl.NumberFormat("pt-BR",{
+              style: 'currency',
+              currency: 'BRL'
+            }).format(product.price)}
+          </span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {/* {cartItemsAmount[product.id] || 0} */} 2
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 };
